15698c85a914ef6f97c83864c07cfbecfaec07fa
Alfonso Iván Almanza González <ivan.almanza.g@gmail.com>
2023-02-04 04:29:21 -0600

    Characters Behaviour

---
+++ Assets/Scripts/CharacterScripts/Character.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    public enum CharacterType 
+    {
+        GNOME = 1,
+        MANDRAKE = 2
+    }
+    protected CharacterType character;
+    private Vector3 movementDirection;
+    public float initalSpeed = 5f;
+    protected float moveSpeed;
+    protected Rigidbody characterBody;
+    protected int life = 2;
+    protected bool canMove;
+    protected int number = 0;
+    protected bool pastThreshold;
+    private Vector3 enemyDirection;
+
+    public int Life
+    {
+        get{return life;}
+        set{life = value;}
+    }
+
+    public int Number 
+    {
+        get{return number;}
+        set{number = value;}
+    }
+
+    public void Awake()
+    {
+        characterBody = this.GetComponent<Rigidbody>();
+        movementDirection = new Vector3(1,0,0);
+        moveSpeed = initalSpeed;
+        if(character == CharacterType.MANDRAKE)
+        {
+            moveSpeed *= -1f;
+        }
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CharacterMovement();
+    }
+
+    void CharacterMovement()
+    {
+        if(canMove && !pastThreshold)
+        {
+            characterBody.velocity = new Vector3(movementDirection.x * moveSpeed, characterBody.velocity.y, characterBody.velocity.z);
+        }
+        else if(pastThreshold)
+        {
+            characterBody.velocity = new Vector3(enemyDirection.x * moveSpeed, characterBody.velocity.y, enemyDirection.z * moveSpeed);
+        }
+    }
+
+    protected void MoveTowardsEnemy(Vector3 _enemyPosition)
+    {
+        enemyDirection = _enemyPosition - this.transform.position;
+        pastThreshold = true;
+    }
+
+    protected void StopMovement()
+    {
+        canMove = false;
+        characterBody.velocity = Vector3.zero;
+    }
+
+    void OnCollisionEnter(Collision _collision)
+    {
+        CharacterCollided(_collision);
+    }
+
+    protected void Die()
+    {
+        StopMovement();
+        gameObject.SetActive(false);
+    }
+
+    public abstract void Damage();
+
+    public abstract void CharacterCollided(Collision collision);
+}
