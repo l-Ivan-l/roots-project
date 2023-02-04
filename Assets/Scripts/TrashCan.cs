4f7c55bac5c76f7fa7c903003166eb0033ab87dc
KatStyles <mashiro.shiina.storm@gmail.com>
2023-02-04 10:55:43 -0600

    Trashcan

---
+++ Assets/Scripts/TrashCan.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCan : MonoBehaviour, IDragable
+{
+    public void ActionDrop(Card _card)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void EndDrag(Transform target)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void StartDrag(Transform refPos)
+    {
+        //throw new System.NotImplementedException();
+    }
+}
