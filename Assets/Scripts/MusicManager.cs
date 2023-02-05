00112eac94ec2b070ec1d2f48beee057fed0224c
Alfonso Iván Almanza González <ivan.almanza.g@gmail.com>
2023-02-05 06:58:57 -0600

    music

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicManager : MonoBehaviour
+public class MusicManager : MonoBehaviour, EventListener<GameEvents>
 {
     [SerializeField] private AudioSource source;
     [SerializeField] 
     [Range(0,1)]private float maxVolume = 0.5f;
+    [Header("Music")]
+    [SerializeField] private AudioClip winMusic;
+    [SerializeField] private AudioClip loseMusic;
 
     [Header("Ambient sounds")]
     [SerializeField] private List<AudioClip> ambientSounds = new List<AudioClip>();
@@ -18,6 +21,16 @@ public class MusicManager : MonoBehaviour
     {
         StartCoroutine(LerpVolume(5,0,maxVolume));
         PlayAmbientSoundInRandomTime();
+    }
+
+    void OnEnable()
+    {
+        this.EventStartListening<GameEvents>();
+    }
+
+    void OnDisable()
+    {
+        this.EventStopListening<GameEvents>();
     }
 
     IEnumerator LerpVolume(float lerpDuration, float startValue, float endValue) 
@@ -47,5 +60,36 @@ public class MusicManager : MonoBehaviour
         source.PlayOneShot(ambientSounds[ran],1);
 
         PlayAmbientSoundInRandomTime();
+    }
+
+    IEnumerator CoChangeMusic(AudioClip newMusic)
+    {
+        StartCoroutine(LerpVolume(1,maxVolume,0));
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(LerpVolume(1,0,maxVolume));
+        source.clip = newMusic;
+        source.Play();
+    }
+
+    public void OnGEvent(GameEvents e)
+    {
+        switch(e.eventType)
+        {
+            case GameEventType.gameOver:
+                StartCoroutine(CoChangeMusic(loseMusic));
+            break;
+
+            case GameEventType.pause:
+                Debug.Log("The game is pause");
+            break;
+
+            case GameEventType.resume:
+                Debug.Log("The game is resume");
+            break;
+
+            case GameEventType.win:
+                StartCoroutine(CoChangeMusic(winMusic));
+            break;
+        }
     }
 }
